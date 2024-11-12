faa163bd87182e42e09d93b772d1d17d861633c5
xronixle <110686455+Zefionth@users.noreply.github.com>
2024-11-12 09:19:59 +0500

    Update recommendations.py

--- recommendations.py
+++ recommendations.py
@@ -10,10 +10,12 @@ def get_recommendations(query: str, category: str) -> list:
     if names:
         if category == 'movies':
             for name in names:
+                if name == None: continue
                 info = get_info_movies(name)
                 recommendations.append(info)
         else:
             for name in names:
+                if name == None: continue
                 info = get_info_anime(name)
                 recommendations.append(info)
 
