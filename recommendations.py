cb835c54df4c69251dba213b8ffdc8d474a3e954
Zefionth <hamitov359@gmail.com>
2025-01-09 13:49:08 +0500

    hello world

--- recommendations.py
+++ recommendations.py
@@ -3,13 +3,13 @@ from kinopoisk_api import get_info_movies
 from shikimoriapi import get_info_anime
 
 def fetch_info(name: str, category: str):
-    """Fetch information based on category."""
+    """Получает информацию на основе категории."""
     if category == 'movies':
         return get_info_movies(name)
     return get_info_anime(name)
 
 def get_recommendations(query: str, category: str) -> list:
-    """Get recommendations based on the query and category."""
+    """Получает рекомендации на основе запроса и категории."""
     recommendations = []
     names = get_names(query, category).values()
 
